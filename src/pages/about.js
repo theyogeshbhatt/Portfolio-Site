c1846dc24970f4ec4e4d7c0dfe34a6319bf800a3
Yogesh Bhatt <yogeshbhattofficial@gmail.com>
2023-06-17 18:35:47 +0530

    Fixed file

--- src/pages/about.js
+++ src/pages/about.js
@@ -53,15 +53,15 @@ const about = () => {
           <h2 className='mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75'>Biography</h2>
           <p className='font-medium'>
 Hi, I am Yogesh Bhatt, a web developer and UI/UX designer with a passion for creating beautiful, functional, 
-and user-centered digital experiences. With 1 years of experience in the field. I am always looking for 
-new and innovative ways to bring my client's visions to life.
+and user:centered digital experiences. With 1 years of experience in the field. I am always looking for 
+new and innovative ways to bring my clients visions to life.
           </p>
           <p className='my-4 font-medium'>
-I believe that design is about more than just making things look pretty – it's about solving problems and 
+I believe that design is about more than just making things look pretty, it is about solving problems and 
 creating intuitive, enjoyable experiences for users. 
           </p>
           <p className='font-medium'>
-Whether I'm working on a website, mobile app, or 
+Whether I am working on a website, mobile app, or 
 other digital product, I bring my commitment to design excellence and user-centered thinking to 
 every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.
           </p>
